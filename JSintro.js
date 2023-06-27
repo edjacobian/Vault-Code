555127d04628e84be66a4d7e1bc77e2c44004bc7
Eddie Jacobian <edjacobian@gmail.com>
2023-06-27 15:25:11 -0400

    added comments appropriately

--- JSintro.js
+++ JSintro.js
@@ -1,16 +1,14 @@
 // Calulate mathematical operators to find each number of code
-// Display vault codes in console
-// Display vault codes in a pop up alert
-
 let number1 = 60 - 50; // 10
 let number2 = 120 / 3; // 40
 let number3 = 19 + 20; // 39
-
+// Display vault codes in console
 console.log("Vault Combination");
 console.log(number1);
 console.log(number2);
 console.log(number3);
-
+// Display welcome alert
 alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination:");
+// DIsplay vault combo alert
 alert("Vault Combination:" + number1 + " - " + number2 + " - " + number3);
 
